10467eae3f919698c14279ae9e73c8d2d9526006
goldenadnane <adnaneabouelmakarim@gmail.com>
2023-05-04 15:57:37 +0100

    commit

---
+++ website/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import *
+
+admin.site.register(Car)
+admin.site.register(Faq)
+admin.site.register(Year)
+admin.site.register(Order)
+admin.site.register(Customer)
+admin.site.register(canceledOrders)
